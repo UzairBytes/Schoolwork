d7b6048f0758582fb87a8e4420a704273b8825bb
Uzair Chudhary <uzair.chudhary@mail.utoronto.ca>
2019-10-04 15:39:42 -0400

    Final tested copy of count_cells program

--- a1/count_cells.c
+++ a1/count_cells.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void read_image(int num_rows, int num_cols, 
                 int arr[num_rows][num_cols], FILE *fp);
                 
-void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]);
+void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]); 
 
 // Remember to include the function prototype for count_cells
-
+int count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
 int main(int argc, char **argv) {
     // Print a message to stderr and exit with an argument of 1 if there are
     // not the right number of parameters, or the second argument is not -p
+    if ((argc != 3 && argc != 2) || (argc == 3 && strcmp(argv[2], "-p") != 0)) {
+        fprintf(stderr, "Usage: count_cells <imagefile.txt> [-p]\n");
+        exit(1);
+    }
+    if (argc == 2){
+        int i, j;
+        FILE* fp = fopen(argv[1], "r");
+        fscanf(fp, "%d %d", &i, &j);
+        int arr[i][j];
+        read_image(i, j, arr, fp);
+        int num_cells = count_cells(i, j, arr);
+        printf("Number of Cells is %d\n", num_cells);
+    }
+    else{
+        int i, j;
+        FILE* fp = fopen(argv[1], "r");
 
-
+        fscanf(fp, "%d %d", &i, &j);
+        int arr[i][j];
+        read_image(i, j, arr, fp);
+        int num_cells = count_cells(i, j, arr);
+        print_image(i, j, arr);
+        printf("Number of Cells is %d\n", num_cells);
+    }
     return 0;
 }
--- a1/image.c
+++ a1/image.c
@@ -5,12 +5,53 @@
  */
 void read_image(int num_rows, int num_cols, 
                 int arr[num_rows][num_cols], FILE *fp) {
-
+    for (int x = 0; x < num_rows; x++){
+        for (int y = 0; y < num_cols; y++){
+            fscanf(fp, "%d", &arr[x][y]);
+        }
+    }
 }
 
 /* Print to standard output the contents of the array arr */
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
+    for (int x = 0; x < num_rows; x++){
+        for (int y = 0; y < num_cols; y++){
+            printf("%d ", arr[x][y]);
+        }
+        printf("\n");
+    }
+}
 
+void eliminate_cell(int x, int y, int num_rows, int num_cols, int arr[num_rows][num_cols]){
+    arr[x][y]=0;
+    
+    if (x-1>= 0 && arr[x-1][y] != 0){
+        eliminate_cell(x-1, y, num_rows, num_cols, arr);
+    }
+    if (x+1 < num_rows && arr[x+1][y]!=0){
+        eliminate_cell(x+1, y, num_rows, num_cols, arr);
+    }
+    if (y-1 >= 0 && arr[x][y-1] != 0){
+        eliminate_cell(x, y-1, num_rows, num_cols, arr);
+    }
+    if (y+1 < num_cols && arr[x][y+1] != 0){
+        eliminate_cell(x, y+1, num_rows, num_cols, arr);
+    }
 }
 
 /* TODO: Write the count_cells function */
+int count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
+    // Count the number of cells by looking for the one pixel of the cell that
+    // has no  non-zero pixel around it.
+    int num_cells = 0;
+    for (int row = 0; row < num_rows; row++){
+        for (int col = 0; col < num_cols; col++){
+            if (arr[row][col] == 255){
+                eliminate_cell(row, col, num_rows, num_cols, arr);
+                num_cells++;
+                }
+        }
+    }
+    return num_cells;
+}
+
