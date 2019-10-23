6fcc13007ac98352374dfea795b9824db542d62a
Uzair Chudhary <uzair.chudhary@mail.utoronto.ca>
2019-10-22 22:14:39 -0400

    Working copy of mytest

---
+++ a2/mytest.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include "smalloc.h"
+
+
+#define SIZE 4096 * 64
+
+
+/* My test of interesting case is checking if it works 
+ * when called a fairly large and prime number of times
+ */
+
+int main(void) {
+
+    mem_init(SIZE);
+    
+    char *ptrs[20];
+    int i;
+
+    /* Call smalloc 7 times */
+    
+    for(i = 0; i < 17; i++) {
+        int num_bytes = (i+1) * 10;
+    
+        ptrs[i] = smalloc(num_bytes);
+        write_to_mem(num_bytes, ptrs[i], i);
+    }
+    printf("List of allocated blocks:\n");
+    print_allocated();
+    printf("List of free blocks:\n");
+    print_free();
+    printf("Contents of allocated memory:\n");
+    print_mem();
+    
+    printf("freeing %p result = %d\n", ptrs[1], sfree(ptrs[1]));
+    
+    printf("List of allocated blocks:\n");
+    print_allocated();
+    printf("List of free blocks:\n");
+    print_free();
+    printf("Contents of allocated memory:\n");
+    print_mem();
+
+    mem_clean();
+    return 0;
+}
